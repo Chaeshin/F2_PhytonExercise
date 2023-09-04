9a1c9241727d1c2e03f2f70fa903f5d845aaea60
fronald143 <120772118+fronald143@users.noreply.github.com>
2023-09-04 14:55:30 +0800

    number 5 continuation

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-# def decToBinary(dec):
-#     while dec > 0:
-#         remainder = quotient % 2
-#         binary_number = str(remainder) + binary_number
-#         quotient = quotient // 2
+def dectobinary(dec):
+    binary_number = ""
+    while dec > 0:
+        rem = dec % 2
+        binary_number = str(rem) + binary_number
+        dec //= 2
+    print(binary_number)
 # def binaryToN(bin, type):
-# def decToOctal(dec):
-# def decToHex(dec):
-#
-# def main():
-#     decToBinary(dec)
+def decToOctal(dec):
+    place = 1
+    oct=0
+    while dec > 0:
+        rem = dec % 8
+        oct = oct + rem * place
+        dec //= 8
+        place *= 10
+    print(oct)
+def decToHex(dec):
+    digits = "0123456789ABCDEF"
+    hex = ""
+
+    while dec > 0:
+        rem = dec % 16
+        hexdigit = digits[rem]
+        hex = hexdigit + hex
+        dec //= 16
+    print(hex)
+def main():
+    dectobin = int(input("Enter decimal to convert for binary:"))
+    dectobinary(dectobin)
 #     binaryToN(bin, type)
-#     decToOctal(dec)
-#     decToHex(dec)
+    dectooct = int(input("Enter decimal to convert for octal:"))
+    decToOctal(dectooct)
+    dectohex = int(input("Enter decimal to convert for hexadecimal:"))
+    decToHex(dectohex)
+
+main()
